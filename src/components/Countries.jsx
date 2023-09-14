255e4074da6f11d375bb55e52d4346c93be93ea4
yuvrajdip <dipbhowmik2018@gmail.com>
2023-09-14 22:34:09 +0600

    conditional css class and conditional style

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -6,6 +6,11 @@ import './Country.css'
 const Countries = () => {
 
   const [countries, setCountries] = useState([])
+  const [visitedCountries , setVisitedCountries] = useState([])
+
+  const handleVisitedCountries = (country) => {
+    setVisitedCountries([...visitedCountries, country])
+  }
 
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
@@ -16,10 +21,13 @@ const Countries = () => {
   return (
     <div>
       <h3>Countries length: {countries.length} </h3>
+      <h3>Visited Countries : {visitedCountries.length}</h3>
       <div className="countries-container">
         {
           countries.map((country, id) => <Country
-            key={id} country={country}>
+            key={id} country={country}
+            handleVisitedCountries={handleVisitedCountries}  
+          >
           </Country>)
         }
       </div>
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({ country, handleVisitedCountries }) => {
   console.log(country);
-  const {currencies, name, flags, population , capital} = country;
+  const { currencies, name, flags, population, capital } = country;
 
   const [visited, setVisit] = useState(false)
 
-  const handleButton = ()=> {
+  const handleButton = () => {
     //* Toggle State based onClick 
     setVisit(!visited) //* toggle between state
+    handleVisitedCountries(country)
   }
 
   return (
-    <div className='country'>
-      <h2>Country: {name.common}</h2>    
+    // * Conditional CSS and conditional style
+    <div className={`country ${visited && 'visited'}`}>
+      <h2 style={{ color: visited ? 'red' : 'yellow' }}>Country: {name.common}</h2>
+
       <img height="100px" src={flags.png} alt="" />
       <p>Population : {population}</p>
-      
+
       <button onClick={handleButton}>{visited ? 'Visited' : 'Going'}</button>
       {/* Conditional rendering */}
       {visited ? 'I have visited' : "Want to visit"}
