30d6fee0e6d7ab6530b0ec3d9b0d492e2e47b089
yuvrajdip <dipbhowmik2018@gmail.com>
2023-09-14 17:22:33 +0600

    toggle state based onClick and conditional rendering

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,22 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import Country from './Country';
+import Country from './Country.jsx'
+import './Countries.css'
+import './Country.css'
 
 const Countries = () => {
-  
+
   const [countries, setCountries] = useState([])
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
-    .then(res=>res.json())
-    .then(data => setCountries(data))
-  },[])
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }, [])
 
   return (
     <div>
       <h3>Countries length: {countries.length} </h3>
-      {
-        countries.map( (country,id)=><Country key={id} country={country}></Country> )
-      }
+      <div className="countries-container">
+        {
+          countries.map((country, id) => <Country
+            key={id} country={country}>
+          </Country>)
+        }
+      </div>
     </div>
   );
 };
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
   console.log(country);
-  const {name} = country;
+  const {currencies, name, flags, population , capital} = country;
+
+  const [visited, setVisit] = useState(false)
+
+  const handleButton = ()=> {
+    //* Toggle State based onClick 
+    setVisit(!visited) //* toggle between state
+  }
 
   return (
-    <div>
+    <div className='country'>
       <h2>Country: {name.common}</h2>    
+      <img height="100px" src={flags.png} alt="" />
+      <p>Population : {population}</p>
+      
+      <button onClick={handleButton}>{visited ? 'Visited' : 'Going'}</button>
+      {/* Conditional rendering */}
+      {visited ? 'I have visited' : "Want to visit"}
     </div>
   );
 };
